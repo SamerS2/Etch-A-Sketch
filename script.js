4d9ab216ff342a187af2c2975edf02481035a76f
SamerS2 <102707222+SamerS2@users.noreply.github.com>
2023-04-04 16:15:34 -0700

    createGrid function created

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+const size = 16;
+
+let defaultSize = size;
+
+document.addEventListener("DOMContentLoaded", function(){
+    createGrid(defaultSize);
+    console.log("Hi");
+})
+
+function createGrid(size){
+    let grid = document.querySelector(".box-container");
+
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs;i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "red";
+        grid.insertAdjacentElement("beforeend", div);
+    }
+}
